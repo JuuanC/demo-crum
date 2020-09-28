fec56524f0974f32be900eb84c32258fa475490f
betoes <ragest031@gmail.com>
2020-09-28 14:51:18 -0500

    Modelos de institucion y hospital

--- src/main/java/model/Institution.java
+++ src/main/java/model/Institution.java
@@ -2,7 +2,6 @@ package model;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -12,23 +11,14 @@ import javax.persistence.Table;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import lombok.Data;
 
-@Entity
 @Data
-@Table(name ="hospital")
-public class Hospital extends PanacheEntityBase {
+@Entity
+@Table(name="institution")
+public class Institution extends PanacheEntityBase{
 	@Id
 	@GeneratedValue(strategy = SEQUENCE, generator = "SEQ_PROD")
     @SequenceGenerator(name="SEQ_PROD", sequenceName = "SEQ_PROD", allocationSize = 1, initialValue = 11)
     private Long id;
 	
-	@Column(unique=true)
 	private String name;
-	
-	private String street;
-	
-	private String streetNumber;
-	
-	private String zipCode;
-	
-	private String Institution;
 }
